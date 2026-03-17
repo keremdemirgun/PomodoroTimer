852c88a51e048fe1d807fd0f69001b1defb862f1
Kerem Demirgun <keremdemirgun@gmail.com>
2026-03-18 00:42:08 +0300

    Logic improvments

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ const timerText = document.getElementById("timeText");
 const startBtn = document.getElementById("startBtn");
 
 let secondsLeft = (Number(timerText.textContent.slice(0,2))) * 60;
+let sayac = null;
 
 
 startBtn.addEventListener("click", () =>{
 
-    setInterval(() => {
+    if(sayac !== null){
+        clearInterval(sayac);
+
+    }
+    
+    
+    sayac = setInterval(() => {
     
         secondsLeft -= 1;
-        minutesLeft = Math.floor(secondsLeft / 60);
-        secondsLeft2 = Math.floor(secondsLeft % 60);
+        let minutesLeft = Math.floor(secondsLeft / 60);
+        let secondsLeft2 = Math.floor(secondsLeft % 60);
 
         if(secondsLeft2<10){
             secondsLeft2 = "0" + secondsLeft2;
+
+                if(minutesLeft == 0 && secondsLeft2 == "00" ){
+                    clearInterval(sayac);
+                    alert("Bitti!");
+                }
+
         }
+        
+
 
         timerText.textContent = minutesLeft + ":" + secondsLeft2;
+
 
     }, 1000);
 
