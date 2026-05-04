1355882fdd82fe6b163e37c34516a5f6caa2f88d
Kerem Demirgun <keremdemirgun@gmail.com>
2026-05-04 23:16:07 +0300

    everytings works perfectly! finaly

--- script.js
+++ script.js
@@ -3,47 +3,49 @@ const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const restartBtn = document.getElementById("restartBtn");
 
-let remaningTime = localStorage.getItem("remaningTime");
+let remaningTime = localStorage.getItem("remaningTime") || "25:00";
 console.log(remaningTime);
-timerText.textContent = remaningTime ? remaningTime : "25:00";
 
-  
-const gorevListesi = document.getElementById("gorev-listesi");
-const gorevText = document.getElementById("gorevText");
-const gorevEkleBtn = document.getElementById("gorevEkleBtn");
+timerText.textContent = remaningTime ? remaningTime : "25:00";
 
 let firstMinute = remaningTime;
 
-let secondsLeft = Number(timerText.textContent.slice(0, 2)) * 60;
+const parts = timerText.textContent.split(":");
+
+let secondsLeft = Number(parts[0]) * 60 + Number(parts[1]);
 let sayac = null;
 
 startBtn.addEventListener("click", () => {
   if (sayac !== null) {
     clearInterval(sayac);
   }
-
+  
   sayac = setInterval(() => {
     secondsLeft -= 1;
     let minutesLeft = Math.floor(secondsLeft / 60);
     let secondsLeft2 = Math.floor(secondsLeft % 60);
 
+    localStorage.setItem("remaningTime", minutesLeft + ":" + secondsLeft2);
+    
     if (secondsLeft2 < 10) {
       secondsLeft2 = "0" + secondsLeft2;
-
+      
       if (minutesLeft == 0 && secondsLeft2 == "00") {
         clearInterval(sayac);
         alert("Bitti!");
       }
     }
-
+    
     timerText.textContent = minutesLeft + ":" + secondsLeft2;
   }, 1000);
 });
 
 restartBtn.addEventListener("click", () => {
   clearInterval(sayac);
-  timerText.textContent = firstMinute;
+  timerText.textContent = "25:00";
   secondsLeft = Number(timerText.textContent.slice(0, 2)) * 60;
+  remaningTime = "25:00";
+  localStorage.setItem("remaningTime", "25:00");
   sayac = null;
 });
 
@@ -53,6 +55,9 @@ stopBtn.addEventListener("click", () => {
   localStorage.setItem("remaningTime", timerText.textContent);
 });
 
+const gorevListesi = document.getElementById("gorev-listesi");
+const gorevText = document.getElementById("gorevText");
+const gorevEkleBtn = document.getElementById("gorevEkleBtn");
 gorevEkleBtn.addEventListener("click", () => {
   let yeniGorev = gorevText.value;
   let gorevsilBtn = document.createElement("button");
